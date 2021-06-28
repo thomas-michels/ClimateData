b7b2504b0645bc04d660083bcf984d02bc0960c3
Thomas Michels Rodrigues <thomas.rodrigues@ambevtech.com.br>
2021-06-28 17:27:25 -0300

    creating loading screen

--- frontend/mainWindow.py
+++ frontend/mainWindow.py
@@ -1,8 +1,13 @@
 import sys
+import time
 from datetime import date
 
 from PyQt5 import QtWidgets, QtGui
+from PyQt5.QtCore import QTimer
+from PyQt5.QtGui import QMovie
+from PyQt5.QtWidgets import QLabel
 
+from frontend.loadingScreen import LoadingScreen
 from frontend.styleSheet import STYLE
 from settings import PROJECT_NAME, INIT_SCREEN_X, INIT_SCREEN_Y, SIZE_X, SIZE_Y
 
@@ -16,24 +21,32 @@ class MainWindow:
         self._font = QtGui.QFont()
         self._font.setPointSize(12)
 
+        self._font_bold = QtGui.QFont()
+        self._font_bold.setPointSize(12)
+        self._font_bold.setBold(True)
+
         self.init_gui()
+        self.load()
 
         self.window.show()
         self.window.setWindowTitle(PROJECT_NAME)
         self.window.setStyleSheet(STYLE)
         self.window.setGeometry(INIT_SCREEN_X, INIT_SCREEN_Y, SIZE_X, SIZE_Y)
 
+        if self.btn_download.pressed():
+            self.start_load(self.movie)
+
         sys.exit(self.app.exec())
 
     def init_gui(self):
 
-        lbl_files = self._create_label("Quantidade de Arquivos", 55, 20, 190, 30)
+        lbl_files = self._create_label("Quantidade de Arquivos", 55, 20, 200, 30, bold=True)
 
-        cb_files = self._create_combo_box(55, 60, 190, 30)
+        cb_files = self._create_combo_box(55, 60, 190, 100)
         cb_files.addItem('1 Arquivo')
         cb_files.addItem('Vários Arquivos')
 
-        lbl_extract = self._create_label("Extração", 55, 100, 190, 30)
+        lbl_extract = self._create_label("Extração", 55, 100, 190, 30, bold=True)
 
         lbl_url = self._create_label('URL', 55, 135, 190, 30)
 
@@ -52,7 +65,23 @@ class MainWindow:
 
         dt_final = self._create_date_edit(140, 260, 190, 30)
 
-        btn_download = self._create_button('BAIXAR', 90, 420, 120, 40)
+        self.btn_download = self._create_button('BAIXAR', 90, 420, 120, 40)
+
+    def load(self):
+        label = QLabel(self.window)
+
+        self.movie = QMovie('frontend/images/loading.gif')
+        label.setMovie(self.movie)
+        label.setScaledContents(True)
+        label.setGeometry(0, 100, 300, 300)
+
+        timer = QTimer(self.window)
+
+    def start_load(self, movie):
+        movie.start()
+
+    def stop_load(self, movie):
+        movie.stop()
 
     def _create_button(self, text, pos_x, pos_y, tam_x, tam_y):
         btn = QtWidgets.QPushButton(text, self.window)
@@ -60,17 +89,20 @@ class MainWindow:
         btn.setFont(self._font)
         return btn
 
-    def _create_label(self, text, pos_x, pos_y, tam_x, tam_y):
+    def _create_label(self, text, pos_x, pos_y, tam_x, tam_y, bold=False):
         lbl = QtWidgets.QLabel(text, self.window)
         lbl.setGeometry(pos_x, pos_y, tam_x, tam_y)
-        lbl.setFont(self._font)
+        if bold:
+            lbl.setFont(self._font_bold)
+        else:
+            lbl.setFont(self._font)
         return lbl
 
     def _create_combo_box(self, pos_x, pos_y, tam_x, tam_y):
         widget = QtWidgets.QWidget(self.window)
         widget.setGeometry(pos_x, pos_y, tam_x, tam_y)
         cb = QtWidgets.QComboBox(widget)
-        cb.setFont(self._font)
+        cb.setFont(self._font_bold)
         return cb
 
     def _create_edit_text(self, pos_x, pos_y, tam_x, tam_y):
@@ -87,3 +119,11 @@ class MainWindow:
         dt.setCalendarPopup(True)
         dt.setFont(self._font)
         return dt
+
+    def _loading(self, path):
+        image = QtGui.QImage(path)
+        display_image = QtGui.QPixmap.fromImage(image)
+        lbl = self._create_label('', 0, 75, 300, 300)
+        lbl.setPixmap(display_image)
+        lbl.setScaledContents(True)
+        return lbl
--- frontend/styleSheet.py
+++ frontend/styleSheet.py
@@ -1,14 +1,25 @@
 STYLE = """
 QWidget {
-    background-color: rgb(233, 233, 233);
+    background-color: white;
 }
 
 QPushButton:hover {
-    background-color: rgb(52, 85, 116);
-    color: rgb(255, 255, 255);
+    background-color: rgb(106, 139, 180);
+    border-radius: 10px;
 }
             
 QPushButton {
-    background-color: rgb(106, 139, 180);
+    background-color: rgb(52, 85, 116);
+    color: rgb(255, 255, 255);
+    border-radius: 10px;
+}
+
+QComboBox {
+    background-color: white;
+    border-radius: 10px;
+}
+
+QComboBox:down-arrow {
+    background-image : url(frontend/images/arrow.png);
 }
 """
