b570f2f324942bfa53af4d0a16ce43e043bae427
Thomas Michels Rodrigues <thomas.rodrigues@ambevtech.com.br>
2021-07-18 15:31:38 -0300

    updating version

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 import os
 
 PROJECT_NAME = 'Climate Data Extractor'
-VERSION = 'By Thomas Michels Rodrigues - v2.0'
+VERSION = 'By Thomas Michels Rodrigues - v2.1'
 INIT_SCREEN_X = 400
 INIT_SCREEN_Y = 100
 SIZE_X = 300
